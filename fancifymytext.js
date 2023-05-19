6e4aab9b41be4f1ef1894e3aec6747df6f9ee9af
jchen42 <91099706+jchen42@users.noreply.github.com>
2023-05-19 16:11:00 -0700

    Assignment 5

---
+++ fancifymytext.js
@@ -0,0 +1,25 @@
+function bigger() {
+    alert("Different Front");
+    document.getElementById("Textbox").style.fontSize = "48pt";
+}
+
+function fancify() {
+
+    document.getElementById("Textbox").style.fontWeight = "Bold";
+    document.getElementById("Textbox").style.color = "Green";
+    document.getElementById("Textbox").style.textDecoration = "underline";
+}
+
+function makeBoring() {
+	document.getElementById("Textbox").style.fontWeight = "normal";
+	document.getElementById("Textbox").style.color = "Black";
+	document.getElementById("Textbox").style.textDecoration = "none";
+
+}
+
+function mooBtn(){
+	var userText = document.getElementById("Textbox");
+	userText.style.textTransform = "uppercase";
+	var parts = userText.value.split(".");
+	userText.value = parts.join("-Moo");
+}
